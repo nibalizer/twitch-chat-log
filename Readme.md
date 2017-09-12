53e920d202aaeffc6676990d238115f9ecc3a523
Spencer Krum <nibz@spencerkrum.com>
2017-09-12 11:12:41 -0500

    Make just a chat log app

--- Readme.md
+++ Readme.md
@@ -1,11 +1,6 @@
-# Twitch Chat Tone Analysis
+# Twitch Chat Log
 
-This is an example application that uses the twitch api, the watson api, socket.io, and rickshaw. It shows the tone of twitch chat accross several tone groups. Using this, a streamer can get a sense of the mood and feeling of chat at a glance.
-
-This also demonstrates using the watson api, the twitch api, and websockets.
-
-
-![Tone Analysis Screenshot](watson-twitch-tone-analysis.png)
+This is an example application that uses the twitch api and logs all communication.
 
 
 ## Installation
--- index.js
+++ index.js
@@ -3,19 +3,9 @@
 var config = require('./config.json');
 
 // Load libraries; instantiate express app and socket.io
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 var tmi = require('tmi.js');
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+var fs = require('fs');
 
-// Setup Watson Tone Analyzer
-var tone_analyzer = new ToneAnalyzerV3({
-  username: config.watson.username,
-  password: config.watson.password,
-  version_date: config.watson.version_date,
-});
 
 // Set up options for connection to twitch chat
 // Add channels in the config.json file
@@ -35,48 +25,13 @@ var tmi_options = {
 var client = new tmi.client(tmi_options);
 client.connect();
 
-// Emit hello world announcement on first chat connection
-client.on('connected', function(address, port) {
-  client.action(config.twitch_channels[0], "Hello World");
-});
 
 
-
-// Serve any files in the 'vendor' directory as static resources
-// This is where js libraries for the client are stored
-app.use(express.static('vendor'))
-
-// Serve index.html on /
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-// Start the web server on port 3000
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-// Setup a websocket with any web client that connects
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-
-// When a chat message comes in, process it with watson tone analysis
-// and send that to any clients connected via websockets
-// This is the meat of the proram
+// When a chat message comes in, write it to a file
 client.on('chat', function(channel, user, message, self) {
-  tone_analyzer.tone({ text: message },
-  function(err, tone) {
-    if (err)
-      console.log(err);
-    else
-      io.emit('chat message', tone['document_tone']);
+  var date = new Date();
+  log_line = date + " " + channel + " " + user.username + " " + message
+  fs.appendFile('twitch_chat_log_' + channel + '.txt', log_line + "\n", function (err) {
+    if (err) throw err;
   });
 });
